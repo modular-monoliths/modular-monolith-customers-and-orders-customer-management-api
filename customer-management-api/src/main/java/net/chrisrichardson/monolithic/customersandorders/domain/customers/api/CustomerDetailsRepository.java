456bcbc21b6174ef69d56c72a422a3b27fad0853
Chris Richardson <chris@chrisrichardson.net>
2020-08-28 03:31:16 -0700

    Removed customer details

--- customer-management-api/src/main/java/net/chrisrichardson/monolithic/customersandorders/domain/customers/api/CustomerDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package net.chrisrichardson.monolithic.customersandorders.domain.customers.api;
-
-import java.util.Optional;
-
-public interface CustomerDetailsRepository {
-
-  Optional<CustomerDetails> findById(long customerId);
-}
